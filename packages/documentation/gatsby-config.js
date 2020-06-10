ce95e1a7641bd505e705ddfb259b0e7ff4f003c1
Bill Fienberg <billfienberg@gmail.com>
2020-06-09 19:22:05 -0500

    Revert "documentation/gatsby-config.js: Try commenting out every gatsby plugin"
    This reverts commit cb099b59d571b1263421f3d8c438f8e28611104b.

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -10,131 +10,133 @@ module.exports = {
   },
   pathPrefix: "/veteran-facing-services-tools",
   plugins: [
-    // {
-    //   resolve: "gatsby-source-git",
-    //   options: {
-    //     name: "va.gov-team",
-    //     remote:
-    //       "https://github.com/department-of-veterans-affairs/va.gov-team.git",
-    //     branch: "master",
-    //     patterns: "platform/working-with-vsp/**",
-    //   },
-    // },
-    // `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `vagov-documentation`,
-    //     short_name: `vagov-documentation`,
-    //     start_url: `/`,
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-mdx`,
-    //   options: {
-    //     extensions: [".mdx", ".md"],
-    //     mediaTypes: ["text/x-markdown"],
-    //     defaultLayouts: {
-    //       default: require.resolve("./src/layouts/SidebarLayout.jsx"),
-    //     },
-    //     gatsbyRemarkPlugins: [
-    //       {
-    //         resolve: `gatsby-remark-images`,
-    //         options: {
-    //           maxWidth: 1035,
-    //           sizeByPixelDensity: true,
-    //         },
-    //       },
-    //       {
-    //         resolve: path.resolve(
-    //           __dirname,
-    //           "./plugins/remark/gatsby-remark-mdx-mermaid"
-    //         ),
-    //       },
-    //       {
-    //         resolve: "gatsby-remark-slug",
-    //       },
-    //     ],
-    //   },
-    // },
-    // `gatsby-plugin-sass`,
-    // `gatsby-plugin-sitemap`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `../formation-react/src/components`,
-    //     name: "components",
-    //   },
-    // },
-    // `gatsby-transformer-react-docgen`,
-    // {
-    //   resolve: `gatsby-transformer-remark`,
-    //   options: {
-    //     // CommonMark mode (default: true)
-    //     commonmark: true,
-    //     // Footnotes mode (default: true)
-    //     footnotes: true,
-    //     // Pedantic mode (default: true)
-    //     pedantic: true,
-    //     // GitHub Flavored Markdown mode (default: true)
-    //     gfm: true,
-    //     // Plugins configs
-    //     plugins: [],
-    //   },
-    // },
+    {
+      resolve: "gatsby-source-git",
+      options: {
+        name: "va.gov-team",
+        remote:
+          "https://github.com/department-of-veterans-affairs/va.gov-team.git",
+        branch: "master",
+        patterns: "platform/working-with-vsp/**",
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `vagov-documentation`,
+        short_name: `vagov-documentation`,
+        start_url: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        mediaTypes: ["text/x-markdown"],
+        defaultLayouts: {
+          default: require.resolve("./src/layouts/SidebarLayout.jsx"),
+        },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1035,
+              sizeByPixelDensity: true,
+            },
+          },
+          {
+            resolve: path.resolve(
+              __dirname,
+              "./plugins/remark/gatsby-remark-mdx-mermaid"
+            ),
+          },
+          {
+            resolve: "gatsby-remark-slug",
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `../formation-react/src/components`,
+        name: "components",
+      },
+    },
+    `gatsby-transformer-react-docgen`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     // `github-api-pages`,
-    // `mdx-pages`,
-    // {
-    //   resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
-    //   options: {
-    //     // Fields to index
-    //     fields: [`title`, `tags`],
-    //     // How to resolve each field`s value for a supported node type
-    //     resolvers: {
-    //       // For any node of type MarkdownRemark, list how to resolve the fields` values
-    //       SitePage: {
-    //         title: (node) => {
-    //           if (
-    //             node.context &&
-    //             node.context.frontmatter &&
-    //             node.context.frontmatter.title
-    //           ) {
-    //             return node.context.frontmatter.title;
-    //           } else if (
-    //             node.context &&
-    //             node.context.sourceUrl &&
-    //             node.context.title
-    //           ) {
-    //             // Search by title derived from documents pulled from the GitHub repo.
-    //             return node.context.title;
-    //           } else {
-    //             console.info("Page title missing from front matter.", node);
-    //           }
-    //           return "";
-    //         },
-    //         tags: (node) => {
-    //           if (
-    //             node.context &&
-    //             node.context.frontmatter &&
-    //             node.context.frontmatter.tags
-    //           ) {
-    //             return node.context.frontmatter.tags;
-    //           }
-    //           return "";
-    //         },
-    //         path: (node) => node.path,
-    //       },
-    //     },
-    //   },
-    // },
+    `mdx-pages`,
+    {
+      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
+      options: {
+        // Fields to index
+        fields: [`title`, `tags`],
+        // How to resolve each field`s value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields` values
+          SitePage: {
+            title: (node) => {
+              if (
+                node.context &&
+                node.context.frontmatter &&
+                node.context.frontmatter.title
+              ) {
+                return node.context.frontmatter.title;
+              } else if (
+                node.context &&
+                node.context.sourceUrl &&
+                node.context.title
+              ) {
+                // Search by title derived from documents pulled from the GitHub repo.
+                return node.context.title;
+              } else {
+                console.info("Page title missing from front matter.", node);
+              }
+
+              return "";
+            },
+            tags: (node) => {
+              if (
+                node.context &&
+                node.context.frontmatter &&
+                node.context.frontmatter.tags
+              ) {
+                return node.context.frontmatter.tags;
+              }
+
+              return "";
+            },
+            path: (node) => node.path,
+          },
+        },
+      },
+    },
   ],
 };
