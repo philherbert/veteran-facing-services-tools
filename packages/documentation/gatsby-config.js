3304d0f190acc7aa39c38376c0e530c377b81265
Theo Bentum <theo@adhocteam.us>
2020-06-11 19:54:46 -0700

    Revert "Disable GFM"
    This reverts commit ae8cff8715ae39c5a595a358cc8f832878c99977.

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -76,7 +76,21 @@ module.exports = {
       },
     },
     `gatsby-transformer-react-docgen`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     // `github-api-pages`,
     `mdx-pages`,
     {
