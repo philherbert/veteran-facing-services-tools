ae8cff8715ae39c5a595a358cc8f832878c99977
Theo Bentum <theo@adhocteam.us>
2020-06-11 19:54:46 -0700

    Disable GFM

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -76,21 +76,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-react-docgen`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
-      },
-    },
+    `gatsby-transformer-remark`,
     // `github-api-pages`,
     `mdx-pages`,
     {
