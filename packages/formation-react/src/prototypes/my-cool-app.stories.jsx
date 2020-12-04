e0dbedafb44909384e511300b3e1cb57bacc5134
Christopher Valarida <chris.valarida@adhocteam.us>
2020-12-04 11:57:32 -0800

    Scaffold basic prototype story

---
+++ packages/formation-react/src/prototypes/my-cool-app.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import Modal from '../components/Modal/Modal';
+
+export default {
+  title: 'Prototypes/My cool app',
+};
+
+export const StartPage = () => {
+  return <p>Your cool app goes here</p>;
+};
