1e824e79fb80a073b12a93948666c29eb4dec20b
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2021-01-15 15:07:53 -0500

    Updated Privacy statement

--- packages/formation-react/src/components/OMBInfo/OMBInfo.jsx
+++ packages/formation-react/src/components/OMBInfo/OMBInfo.jsx
@@ -48,22 +48,22 @@ class OMBInfo extends React.Component {
         information as may be necessary from the school for VA to properly
         process the Veteran’s education claim or to monitor his or her progress
         during training) as identified in the VA system of records,
-        58VA21/22/28, Compensation, Pension, Education, and Vocational
-        Rehabilitation and Employment Records - VA, and published in the Federal
-        Register. Your obligation to respond is required to obtain or retain
-        education benefits. Giving us your SSN account information is voluntary.
-        Refusal to provide your SSN by itself will not result in the denial of
-        benefits. The VA will not deny an individual benefits for refusing to
-        provide his or her SSN unless the disclosure of the SSN is required by a
-        Federal Statute of law enacted before January 1, 1975, and still in
-        effect. The requested information is considered relevant and necessary
-        to determine the maximum benefits under the law. While you do not have
-        to respond, VA cannot process your claim for education assistance unless
-        the information is furnished as required by existing law (38 U.S.C.
-        3471). The responses you submit are considered confidential (38 U.S.C.
-        5701). Any information provided by applicants, recipients, and others
-        may be subject to verification through computer matching programs with
-        other agencies.
+        58VA21/22/28, Compensation, Pension, Education, and Veteran Readiness
+        and Employment Records - VA, and published in the Federal Register. Your
+        obligation to respond is required to obtain or retain education
+        benefits. Giving us your SSN account information is voluntary. Refusal
+        to provide your SSN by itself will not result in the denial of benefits.
+        The VA will not deny an individual benefits for refusing to provide his
+        or her SSN unless the disclosure of the SSN is required by a Federal
+        Statute of law enacted before January 1, 1975, and still in effect. The
+        requested information is considered relevant and necessary to determine
+        the maximum benefits under the law. While you do not have to respond, VA
+        cannot process your claim for education assistance unless the
+        information is furnished as required by existing law (38 U.S.C. 3471).
+        The responses you submit are considered confidential (38 U.S.C. 5701).
+        Any information provided by applicants, recipients, and others may be
+        subject to verification through computer matching programs with other
+        agencies.
       </p>
     </div>
   );
